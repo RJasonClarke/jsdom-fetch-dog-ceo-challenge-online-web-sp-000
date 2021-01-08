df09418a22cbf82720cbfd45d7b2bf1137a49dbf
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-08 17:48:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl)
+.then(response => response.json())
+.then(result => {
+    dogImages(result.message)
+  })
